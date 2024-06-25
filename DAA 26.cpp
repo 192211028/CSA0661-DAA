06e82cbcc24e489762a758763234df4b1f04c759
192211028 <123543116+192211028@users.noreply.github.com>
2024-06-25 13:17:00 +0530

    Add files via upload

--- DAA 26.cpp
+++ DAA 26.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
-int main() {
-   int i, space, rows, k = 0;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i, k = 0) {
-      for (space = 1; space <= rows - i; ++space) {
-         printf("  ");
-      }
-      while (k != 2 * i - 1) {
-         printf("* ");
-         ++k;
-      }
-      printf("\n");
-   }
-   return 0;
+
+void printPattern(int n) {
+    int i, j;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
 }
+
+int main() {
+    int n = 4;
+    printPattern(n);
+    return 0;
+}
+
